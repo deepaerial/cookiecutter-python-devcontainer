6690baf89d5d28f2d7d35aeb11b2eb66ba31fe68
deepaerial <nazarii.oleksiuk@gmail.com>
2022-06-16 23:46:29 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# Cookiecutter Python Dev Container
+
+Modern Python dev environment inside Docker container.
+## Quickstart
+Install `cookiecutter` tool if you don't have one already installed and run following command.
+```shell
+cookiecutter https://github.com/audreyfeldroy/cookiecutter-python-devcontainer.git
+```
+Cookiecutter installation instructions of official [wiki](https://cookiecutter.readthedocs.io/en/stable/installation.html).
+
+## Features
+Dev environment inside container pre-installed with:
+
+* [`poetry`](https://python-poetry.org/) for managing project dependencies
+* Predefined VS Code extensions
+
+List of default VS Code extensions is availabel in `.devcontainer/devcontainer.json` 
+
+
---
+++ tests/test_bake_project.py
@@ -0,0 +1,48 @@
+from cgitb import reset
+from pathlib import Path
+import toml
+from typing import Optional
+from pytest_cookies.plugin import Cookies
+
+
+def get_from_pyproject_toml(pyproject_toml: Path, attr: str) -> Optional[str]:
+    text_data = pyproject_toml.read_text("utf-8")
+    data = toml.loads(text_data)
+    for key in attr.split("."):
+        data = data.get(key)
+        if data is None:
+            break
+    return data
+
+
+def test_bake_with_specified_python_version(cookies: Cookies):
+    result = cookies.bake(extra_context={"python_version": "3.9"})
+    assert result.exit_code == 0
+    pyproject_toml = result.project / "pyproject.toml"
+    python_version = get_from_pyproject_toml(
+        pyproject_toml, "tool.poetry.dependencies.python"
+    )
+    assert python_version == "^3.9"
+
+
+def test_bake_with_specified_project_version(cookies: Cookies):
+    result = cookies.bake(extra_context={"version": "1.0.0"})
+    assert result.exit_code == 0
+    pyproject_toml = result.project / "pyproject.toml"
+    project_version = get_from_pyproject_toml(
+        pyproject_toml, "tool.poetry.version"
+    )
+    assert project_version == "1.0.0"
+
+
+def test_bake_with_specified_project_name(cookies: Cookies):
+    project_name = "Project Blue Book"
+    result = cookies.bake(extra_context={"project_name": project_name})
+    assert result.exit_code == 0
+    pyproject_toml = result.project / "pyproject.toml"
+    project_name = get_from_pyproject_toml(
+        pyproject_toml, "tool.poetry.name"
+    )
+    assert project_name == "project_blue_book"
+    project_package = result.project / "project_blue_book"
+    assert project_package.isdir()
